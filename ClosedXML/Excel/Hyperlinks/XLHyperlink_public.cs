silviogneto/ClosedXML

Use String.Concat instead of String.Format to speed up string concatenation

ClosedXML/Excel/Hyperlinks/XLHyperlink_public.cs
@@ -75,14 +75,18 @@ namespace ClosedXML.Excel
                 if (_internalAddress.Contains('!'))
                 {
                     return _internalAddress[0] != '\''
-                               ? String.Format("{0}!{1}",
+                               ? String.Concat(
                                     _internalAddress
                                         .Substring(0, _internalAddress.IndexOf('!'))
                                         .WrapSheetNameInQuotesIfRequired(),
+                                    '!',
                                     _internalAddress.Substring(_internalAddress.IndexOf('!') + 1))
                                : _internalAddress;
                 }
-                return String.Format("{0}!{1}", Worksheet.Name.WrapSheetNameInQuotesIfRequired(), _internalAddress);
+                return String.Concat(
+                    Worksheet.Name.WrapSheetNameInQuotesIfRequired(),
+                    '!',
+                    _internalAddress);
             }
             set
             {

ClosedXML/Excel/Ranges/XLRangeBase.cs
@@ -1581,7 +1581,12 @@ namespace ClosedXML.Excel
 
         public override string ToString()
         {
-            return String.Format("{0}!{1}:{2}", Worksheet.Name.WrapSheetNameInQuotesIfRequired(), RangeAddress.FirstAddress, RangeAddress.LastAddress);
+            return String.Concat(
+                Worksheet.Name.WrapSheetNameInQuotesIfRequired(),
+                '!',
+                RangeAddress.FirstAddress,
+                ':',
+                RangeAddress.LastAddress);
         }
 
         protected void ShiftColumns(IXLRangeAddress thisRangeAddress, XLRange shiftedRange, int columnsShifted)

XLibur/XLibur

ClosedXML/Excel/Hyperlinks/XLHyperlink_public.cs
@@ -75,14 +75,18 @@ namespace ClosedXML.Excel
                 if (_internalAddress.Contains('!'))
                 {
                     return _internalAddress[0] != '\''
-                               ? String.Format("{0}!{1}",
+                               ? String.Concat(
                                     _internalAddress
                                         .Substring(0, _internalAddress.IndexOf('!'))
                                         .EscapeSheetName(),
+                                    '!',
                                     _internalAddress.Substring(_internalAddress.IndexOf('!') + 1))
                                : _internalAddress;
                 }
-                return String.Format("{0}!{1}", Worksheet.Name.EscapeSheetName(), _internalAddress);
+                return String.Concat(
+                    Worksheet.Name.EscapeSheetName(),
+                    '!',
+                    _internalAddress);
             }
             set
             {

ClosedXML/Excel/Ranges/XLRangeBase.cs
@@ -1505,7 +1505,12 @@ namespace ClosedXML.Excel
 
         public override string ToString()
         {
-            return String.Format("{0}!{1}:{2}", Worksheet.Name.EscapeSheetName(), RangeAddress.FirstAddress, RangeAddress.LastAddress);
+            return String.Concat(
+                Worksheet.Name.EscapeSheetName(),
+                '!',
+                RangeAddress.FirstAddress,
+                ':',
+                RangeAddress.LastAddress);
         }
 
         protected void ShiftColumns(IXLRangeAddress thisRangeAddress, XLRange shiftedRange, int columnsShifted)
@@ -1519,7 +1524,7 @@ namespace ClosedXML.Excel
                 return;
 
             bool shiftLeftBoundary = (columnsShifted > 0 && thisRangeAddress.FirstAddress.ColumnNumber >= shiftedRange.RangeAddress.FirstAddress.ColumnNumber) ||
-                                     (columnsShifted < 0 && thisRangeAddress.FirstAddress.ColumnNumber >  shiftedRange.RangeAddress.FirstAddress.ColumnNumber);
+                                     (columnsShifted < 0 && thisRangeAddress.FirstAddress.ColumnNumber > shiftedRange.RangeAddress.FirstAddress.ColumnNumber);
 
             bool shiftRightBoundary = thisRangeAddress.LastAddress.ColumnNumber >= shiftedRange.RangeAddress.FirstAddress.ColumnNumber;
 
@@ -1564,13 +1569,13 @@ namespace ClosedXML.Excel
             if (thisRangeAddress.IsInvalid || shiftedRange.RangeAddress.IsInvalid) return;
 
             bool allColumnsAreCovered = thisRangeAddress.FirstAddress.ColumnNumber >= shiftedRange.RangeAddress.FirstAddress.ColumnNumber &&
-                                        thisRangeAddress.LastAddress.ColumnNumber  <= shiftedRange.RangeAddress.LastAddress.ColumnNumber;
+                                        thisRangeAddress.LastAddress.ColumnNumber <= shiftedRange.RangeAddress.LastAddress.ColumnNumber;
 
             if (!allColumnsAreCovered)
                 return;
 
             bool shiftTopBoundary = (rowsShifted > 0 && thisRangeAddress.FirstAddress.RowNumber >= shiftedRange.RangeAddress.FirstAddress.RowNumber) ||
-                                    (rowsShifted < 0 && thisRangeAddress.FirstAddress.RowNumber >  shiftedRange.RangeAddress.FirstAddress.RowNumber);
+                                    (rowsShifted < 0 && thisRangeAddress.FirstAddress.RowNumber > shiftedRange.RangeAddress.FirstAddress.RowNumber);
 
             bool shiftBottomBoundary = thisRangeAddress.LastAddress.RowNumber >= shiftedRange.RangeAddress.FirstAddress.RowNumber;
 
@@ -1706,7 +1711,6 @@ namespace ClosedXML.Excel
             }
 
             return sb.ToString();
-
         }
 
         public IXLRangeBase Sort()
@@ -1719,7 +1723,6 @@ namespace ClosedXML.Excel
             SortRangeRows();
             return this;
         }
-
 
         public IXLRangeBase Sort(String columnsToSortBy, XLSortOrder sortOrder = XLSortOrder.Ascending, Boolean matchCase = false, Boolean ignoreBlanks = true)
         {