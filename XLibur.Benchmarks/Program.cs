1f626d3d36f509c876efe4b5cadd847b4e96731e
Jason Finch <jason.finch@gmail.com>
2026-03-30 08:41:14 +1000

    fix: register V1 font engine in benchmarks project
    Add SixLaborsV1FontBootstrap.Register() call and V1 project reference.
    Alias OpenXml Spreadsheet.Fonts to resolve namespace conflict with
    XLibur.Fonts.

--- XLibur.Benchmarks/Program.cs
+++ XLibur.Benchmarks/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using BenchmarkDotNet.Running;
 using XLibur.Benchmarks;
+using XLibur.Fonts.SixLabors.V1;
+
+SixLaborsV1FontBootstrap.Register();
 
 if (args.Length > 0 && args[0].Equals("profile", StringComparison.OrdinalIgnoreCase))
 {
